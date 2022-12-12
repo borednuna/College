9a7c0a6db13f6ea120264e6619e8604841aaed0a
borednuna <hanunshaka02@gmail.com>
2022-12-12 16:56:15 +0700

    feat: add queries for final project

---
+++ SBD/insert_karyawan.sql
@@ -0,0 +1,5 @@
+USE LAUNDRY;
+
+INSERT INTO KARYAWAN VALUE('K0001', 'Shani', '0875879834563', 'Gebang Putih', 'Customer Service');
+INSERT INTO KARYAWAN VALUE('K0002', 'Ashel', '0875812345563', 'Gebang Lor', 'Operator Setrika');
+INSERT INTO KARYAWAN VALUE('K0003', 'Eli', '0875812345987', 'Jl Kertajaya', 'Operator Mesin Cuci');
